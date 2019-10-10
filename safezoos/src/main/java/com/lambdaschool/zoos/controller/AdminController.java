066e858cb2a8fc31fd4744cdfc39b9739a46b8d7
Ahmad Guenoun <amguenoun@gmail.com>
2019-10-10 17:02:17 -0400

    created endpoint to get count

--- safezoos/src/main/java/com/lambdaschool/zoos/controller/AdminController.java
+++ safezoos/src/main/java/com/lambdaschool/zoos/controller/AdminController.java
@@ -16,69 +16,48 @@ import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/admin")
-public class AdminController
-{
-    @Autowired
-    ZooService zooService;
+public class AdminController {
+	@Autowired
+	ZooService zooService;
 
-    @PutMapping(value = "/zoos/{id}",
-                produces = {"application/json"},
-                consumes = {"application/json"})
-    public ResponseEntity<?> updateZoo(
-            @RequestBody
-                    Zoo updateZoo,
-            @PathVariable
-                    long id)
-    {
-        zooService.update(updateZoo, id);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+	@PutMapping(value = "/zoos/{id}", produces = {"application/json"}, consumes = {"application/json"})
+	public ResponseEntity<?> updateZoo(@RequestBody Zoo updateZoo, @PathVariable long id) {
+		zooService.update(updateZoo, id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
-    // POST localhost:2019/admin/zoos
-    @PostMapping(value = "/zoos",
-                 consumes = {"application/json"},
-                 produces = {"application/json"})
-    public ResponseEntity<?> addNewZoo(HttpServletRequest request, @Valid
-    @RequestBody
-            Zoo newZoo) throws URISyntaxException
-    {
-        newZoo = zooService.save(newZoo);
+	// POST localhost:2019/admin/zoos
+	@PostMapping(value = "/zoos", consumes = {"application/json"}, produces = {"application/json"})
+	public ResponseEntity<?> addNewZoo(HttpServletRequest request, @Valid @RequestBody Zoo newZoo) throws URISyntaxException {
+		newZoo = zooService.save(newZoo);
 
-        // set the location header for the newly created resource
-        HttpHeaders responseHeaders = new HttpHeaders();
-        // URI newRestaurantURI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{zooid}").buildAndExpand(newZoo.getZooid()).toUri();
-        URI newRestaurantURI = ServletUriComponentsBuilder.fromUriString(request.getServerName() + ":" + request.getLocalPort() + "/zoos/zoos/{zooid}").buildAndExpand(newZoo.getZooid()).toUri();
-        responseHeaders.setLocation(newRestaurantURI);
+		// set the location header for the newly created resource
+		HttpHeaders responseHeaders = new HttpHeaders();
+		// URI newRestaurantURI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{zooid}").buildAndExpand(newZoo.getZooid()).toUri();
+		URI newRestaurantURI = ServletUriComponentsBuilder.fromUriString(request.getServerName() + ":" + request.getLocalPort() + "/zoos/zoos/{zooid}").buildAndExpand(newZoo.getZooid()).toUri();
+		responseHeaders.setLocation(newRestaurantURI);
 
-        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
-    }
+		return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+	}
 
 
-    @DeleteMapping(value = "/zoos/{zooid}")
-    public ResponseEntity<?> deleteZooById(
-            @PathVariable
-                    long zooid)
-    {
-        zooService.delete(zooid);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+	@DeleteMapping(value = "/zoos/{zooid}")
+	public ResponseEntity<?> deleteZooById(@PathVariable long zooid) {
+		zooService.delete(zooid);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
-    @PostMapping(value = "/zoos/{zooid}/animals/{animalid}")
-    public ResponseEntity<?> saveZooAnimalCombo(HttpServletRequest request,
-            @PathVariable("zooid")
-                    long zooid,
-            @PathVariable("animalid")
-                    long animalid)
-    {
-//        zooService.saveZooAnimalCombo(zooid, animalid);
+	@PostMapping(value = "/zoos/{zooid}/animals/{animalid}")
+	public ResponseEntity<?> saveZooAnimalCombo(HttpServletRequest request, @PathVariable("zooid") long zooid, @PathVariable("animalid") long animalid) {
+		//        zooService.saveZooAnimalCombo(zooid, animalid);
 
-        // set the location header for the newly created resource
-        HttpHeaders responseHeaders = new HttpHeaders();
-        // URI newZooURI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{zooid}").buildAndExpand(newZoo.getZooid()).toUri();
-        URI newZooURI = ServletUriComponentsBuilder.fromUriString(request.getServerName() + ":" + request.getLocalPort() + "/zoos/zoos/{zooid}").buildAndExpand(zooid).toUri();
-        responseHeaders.setLocation(newZooURI);
+		// set the location header for the newly created resource
+		HttpHeaders responseHeaders = new HttpHeaders();
+		// URI newZooURI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{zooid}").buildAndExpand(newZoo.getZooid()).toUri();
+		URI newZooURI = ServletUriComponentsBuilder.fromUriString(request.getServerName() + ":" + request.getLocalPort() + "/zoos/zoos/{zooid}").buildAndExpand(zooid).toUri();
+		responseHeaders.setLocation(newZooURI);
 
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
 }
--- safezoos/src/main/java/com/lambdaschool/zoos/repository/AnimalRepository.java
+++ safezoos/src/main/java/com/lambdaschool/zoos/repository/AnimalRepository.java
@@ -9,4 +9,10 @@ import java.util.ArrayList;
 public interface AnimalRepository extends CrudRepository<Animal, Long>
 {
     Animal findByAnimaltype(String type);
+
+    @Query(value = "SELECT COUNT(*) as count FROM ZOOANIMALS WHERE animalid = :animalid", nativeQuery = true)
+    long countAnimal(long animalid);
+
+    @Query(value = "SELECT * FROM animal", nativeQuery = true)
+    ArrayList<Animal> getAnimals();
 }
